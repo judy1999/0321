6b655f0213754097d14f1b79e79c8369daa7fbf5
user <snsdlove91724@gmail.com>
2022-03-21 10:02:42 +0800

    1

---
+++ 0321/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0321
+{
+    class Car
+    {
+        string color = "red";
+        int cc = 1500;
+        string brand = "bmw";
+    public void StartEngin()
+    {
+            Console.WriteLine("發動引擎！");
+
+    }
+        public void ShoInfo()
+        {
+            Console.WriteLine("顏色是：" + color);
+            Console.WriteLine("cc數：" + cc);
+            Console.WriteLine("品牌" + brand);
+        }
+    }
+    
+}
---
+++ 0321/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static private int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
