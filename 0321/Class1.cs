dfb5fe441aac46e009138bcb286bfda8e0b2e9bf
user <snsdlove91724@gmail.com>
2022-03-21 11:27:52 +0800

    3

---
+++ 0321/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0321
+{
+    class Class1:Interface1
+    {
+        public void UploadFile()
+        {
+            Console.WriteLine("上傳！");
+        }
+
+    }
+}
